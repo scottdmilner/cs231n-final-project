fcbce3734a0227bde230371b8e3c95bb234a1d72
Scott Milner <scottdmilner@gmail.com>
2026-05-19 14:03:53 -0700

    add cs231nfinal module, initial dataset management

--- data/CONTENTS.md
+++ data/CONTENTS.md
@@ -8,14 +8,14 @@ data/
 │   ├── smoke_highres.4.vdb
 │   ├── ...
 │   └── smoke_highres.81.vdb
-├── smoke_lowres  # (100, 100, 100) .2
+├── smoke_lowres  # (100, 100, 100)
 │   ├── smoke_lowres.1.vdb
 │   ├── smoke_lowres.2.vdb
 │   ├── smoke_lowres.3.vdb
 │   ├── smoke_lowres.4.vdb
 │   ├── ...
 │   └── smoke_lowres.120.vdb
-└── smoke_midres # (800, 800, 800) .05
+└── smoke_midres # (400, 400, 400)
 │   ├── smoke_midres.1.vdb
 │   ├── smoke_midres.2.vdb
 │   ├── smoke_midres.3.vdb
---
+++ src/cs231nfinal/data.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from enum import StrEnum
+from pathlib import Path
+from typing import TYPE_CHECKING, cast
+
+import openvdb as vdb
+
+if TYPE_CHECKING:
+    GridType = vdb.BoolGrid | vdb.FloatGrid | vdb.Vec3SGrid
+
+
+class DatasetID(StrEnum):
+    SMOKE_LOWRES = "smoke_lowres"
+    SMOKE_MIDRES = "smoke_midres"
+    SMOKE_HIGHRES = "smoke_highres"
+
+
+DATASET_RES = {
+    DatasetID.SMOKE_LOWRES: (100, 100, 100),
+    DatasetID.SMOKE_MIDRES: (400, 400, 400),
+    DatasetID.SMOKE_HIGHRES: (1000, 1000, 1000),
+}
+
+DATASET_VOXEL_SIZE = {
+    DatasetID.SMOKE_LOWRES: 0.2,
+    DatasetID.SMOKE_MIDRES: 0.05,
+    DatasetID.SMOKE_HIGHRES: 0.02,
+}
+
+
+class DataSet:
+    count: int
+    dataset: DatasetID
+    path: Path
+
+    _instances: dict[DatasetID, DataSet] = {}
+
+    def __init__(self, dataset: DatasetID) -> None:
+        self.dataset = dataset
+        self.path = Path(f"data/{dataset}")
+        self.count = len(list(self.path.glob("*.vdb")))
+
+    @classmethod
+    def Get(cls, dataset: DatasetID) -> DataSet:
+        if dataset not in cls._instances:
+            cls._instances[dataset] = DataSet(dataset)
+        return cls._instances[dataset]
+
+    def get_grid[GT: GridType](
+        self, grid_name: str, frame: int, *, type: type[GT]
+    ) -> GT:
+        frame_path = str(self.path / f"{self.dataset}.{frame}.vdb")
+        return cast(GT, vdb.read(frame_path, grid_name))
+
+    @property
+    def resolution(self) -> tuple[int, int, int]:
+        return DATASET_RES[self.dataset]
+
+    @property
+    def voxel_size(self) -> float:
+        return DATASET_VOXEL_SIZE[self.dataset]
+
+
+__all__ = ["DatasetID", "DataSet"]
