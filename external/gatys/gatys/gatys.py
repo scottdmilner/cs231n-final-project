7687e9b19974efddfc7fb50a69b2dc5bb6fb5cc0
Scott Milner <scottdmilner@gmail.com>
2026-06-03 20:35:26 -0700

    gatys cleanup

--- external/gatys/gatys/gatys.py
+++ external/gatys/gatys/gatys.py
@@ -5,7 +5,6 @@ Collected from notebook form into single Python file, tweaked for modern
 pytorch.
 """
 
-import os 
 from pathlib import Path
 
 import torch
@@ -95,31 +94,15 @@ class GramMSELoss(nn.Module):
         return(out)
     
 
-# pre and post processing for images
-# img_size = 512 
-# prep = transforms.Compose([transforms.Resize(img_size),
-#                            transforms.ToTensor(),
-#                            transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
-#                            transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961], #subtract imagenet mean
-#                                                 std=[1,1,1]),
-#                            transforms.Lambda(lambda x: x.mul_(255)),
-#                           ])
 postpa = transforms.Compose([transforms.Lambda(lambda x: x.mul_(1./255)),
                            transforms.Normalize(mean=[-0.40760392, -0.45795686, -0.48501961], #add imagenet mean
                                                 std=[1,1,1]),
                            transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to RGB
                            ])
-# def postp(tensor): # to clip results in the range [0,1]
-#     t = postpa(tensor)
-#     t[t>1] = 1    
-#     t[t<0] = 0
-#     img = postpb(t)
-#     return img
 
 
 class Gatys:
-
-    def prep(self, img: torch.Tensor):
+    def _prep(self, img: torch.Tensor):
         t = transforms.Compose([
             transforms.Resize(self.size),
             transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
@@ -129,7 +112,7 @@ class Gatys:
         ])
         return t(img).unsqueeze(0)
     
-    def postp(self, img: torch.Tensor):
+    def _postp(self, img: torch.Tensor):
         t = postpa(img)
         t[t>1] = 1    
         t[t<0] = 0
@@ -147,7 +130,7 @@ class Gatys:
 
         self.vgg.to(device)
 
-        self.style_image = self.prep(transforms.ToTensor()(Image.open(style_img_path))).to(self.device)
+        self.style_image = self._prep(transforms.ToTensor()(Image.open(style_img_path))).to(self.device)
 
         #define layers, loss functions, weights and compute optimization targets
         self.style_layers = ['r11','r21','r31','r41', 'r51'] 
@@ -165,7 +148,7 @@ class Gatys:
         self.style_targets = [GramMatrix()(A).detach() for A in self.vgg(self.style_image, self.style_layers)]
     
     def do_style(self, content_img: torch.Tensor, max_iter: int = 500):
-        content_image = self.prep(content_img).to(self.device)
+        content_image = self._prep(content_img).to(self.device)
         opt_img = Variable(content_image.data.clone(), requires_grad=True)
 
         content_targets = [A.detach() for A in self.vgg(content_image, self.content_layers)]
@@ -192,64 +175,5 @@ class Gatys:
             
             optimizer.step(closure)
 
-        out_img = self.postp(opt_img.detach().data[0].cpu().squeeze())
+        out_img = self._postp(opt_img.detach().data[0].cpu().squeeze())
         return out_img
-
-
-# def main():
-    # #load images, ordered as [style_image, content_image]
-    # img_dirs = [image_dir, image_dir]
-    # img_names = ['vangogh_starry_night.jpg', 'Tuebingen_Neckarfront.jpg']
-    # imgs = [Image.open(img_dirs[i] + name) for i,name in enumerate(img_names)]
-    # imgs_torch = [prep(img) for img in imgs]
-
-    # device = torch.device("mps")
-
-    # imgs_torch = [Variable(img.unsqueeze(0).to(device)) for img in imgs_torch]
-
-    # style_image, content_image = imgs_torch
-
-    # # opt_img = Variable(torch.randn(content_image.size()).type_as(content_image.data), requires_grad=True) #random init
-    # opt_img = Variable(content_image.data.clone(), requires_grad=True)
-
-    #define layers, loss functions, weights and compute optimization targets
-    # style_layers = ['r11','r21','r31','r41', 'r51'] 
-    # content_layers = ['r42']
-    # loss_layers = style_layers + content_layers
-    # loss_fns = [GramMSELoss()] * len(style_layers) + [nn.MSELoss()] * len(content_layers)
-    # loss_fns = [loss_fn.to(device) for loss_fn in loss_fns]
-        
-    # #these are good weights settings:
-    # style_weights = [1e3/n**2 for n in [64,128,256,512,512]]
-    # content_weights = [1e0]
-    # weights = style_weights + content_weights
-
-    # #compute optimization targets
-    # style_targets = [GramMatrix()(A).detach() for A in vgg(style_image, style_layers)]
-    # content_targets = [A.detach() for A in vgg(content_image, content_layers)]
-    # targets = style_targets + content_targets
-
-    # #run style transfer
-    # max_iter = 50
-    # show_iter = 50
-    # optimizer = optim.LBFGS([opt_img]);
-    # n_iter=[0]
-
-    # while n_iter[0] <= max_iter:
-
-    #     def closure():
-    #         optimizer.zero_grad()
-    #         out = vgg(opt_img, loss_layers)
-    #         layer_losses = torch.stack([weights[a] * loss_fns[a](A, targets[a]) for a,A in enumerate(out)])
-    #         loss = layer_losses.sum()
-    #         loss.backward()
-    #         n_iter[0]+=1
-
-    #         if n_iter[0]%show_iter == (show_iter-1):
-    #             print('Iteration: %d, loss: %f'%(n_iter[0]+1, loss.item()))
-    #         return loss
-        
-    #     optimizer.step(closure)
-
-    # out_img = postp(opt_img.detach().data[0].cpu().squeeze())
-    # return out_img
